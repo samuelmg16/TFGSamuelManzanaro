78aed97b755941db2ec834f642edbc7c3adae9bd
samuelmg16 <samuel19102003@gmail.com>
2024-05-09 15:45:25 +0200

    Cambios inclusion de bootstrap

--- plantillas/header.php
+++ plantillas/header.php
@@ -6,7 +6,7 @@
     <head>
         <title>GameScore | <?php echo $title; ?></title>
         <link rel="stylesheet" href="css/main.css" type="text/css">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     </head>
 
     <body>
